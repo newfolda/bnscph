125b0781cd83de56db4b8287cb7371ed388db614
red <destinydn26@gmail.com>
2026-07-27 13:03:04 +0800

    Update latest transactions

--- src/data/latestTransactions.ts
+++ src/data/latestTransactions.ts
@@ -1,4 +1,7 @@
 export const latestTransactions = [
   { year: "2016", brand: "Mazda", model: "MX-5 Miata", location: "Cavite City", imagePath: "/images/transactions/2016 Mazda MX-5.webp", purchasedAt: "2026-03-15" },
   { year: "2012", brand: "BMW", model: "520d", location: "San Pablo City", imagePath: "/images/transactions/2012 BMW 5 Series 520d.webp", purchasedAt: "2026-03-24" },
+  { year: "2023", brand: "FORD", model: "RANGER RAPTOR", location: "Baguio City", imagePath: "/images/transactions/2023 Ford Ranger Raptor.webp", purchasedAt: "2026-04-08" },
+  { year: "2019", brand: "Mercedes-Benz", model: "C 180", location: "Quezon City", imagePath: "/images/transactions/2019 Mercedes-Benz C 180.webp", purchasedAt: "2026-04-08" },
 ]
+
