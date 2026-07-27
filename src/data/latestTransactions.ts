7e7e5549b6cca657fc8a5940022df0550a149de5
red <destinydn26@gmail.com>
2026-07-27 12:44:52 +0800

    Update latest transactions

--- src/data/latestTransactions.ts
+++ src/data/latestTransactions.ts
@@ -1,4 +1,4 @@
 export const latestTransactions = [
   { year: "2016", brand: "Mazda", model: "MX-5 Miata", location: "Cavite City", imagePath: "/images/transactions/2016 Mazda MX-5.webp", purchasedAt: "2026-03-15" },
-  { year: "2012", brand: "BMW", model: "520d", location: "San Pablo City", imagePath: "/images/transactions/2012 BMW 5 Series 520d", purchasedAt: "2026-03-15" },
+  { year: "2012", brand: "BMW", model: "520d", location: "San Pablo City", imagePath: "/images/transactions/2012 BMW 5 Series 520d.webp", purchasedAt: "2026-03-24" },
 ]
