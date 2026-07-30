2dd3cd673d4b1b4268286dc226bf78f3996bdec9
red <destinydn26@gmail.com>
2026-07-30 17:24:14 +0800

    Add daily transaction images

--- src/data/latestTransactions.ts
+++ src/data/latestTransactions.ts
@@ -1,6 +1,6 @@
 export const latestTransactions = [
   { year: "2016", brand: "MAZDA", model: "MX-5 Miata", location: "Cavite City", imagePath: "/images/transactions/2016 Mazda MX-5.webp", purchasedAt: "2026-05-15" },
-  { year: "2020", brand: "FORD", model: "RANGER XLT", location: "San Pablo City", imagePath: "/images/transactions/2012 2020 Ford Ranger XLT.webp", purchasedAt: "2026-05-24" },
+  { year: "2020", brand: "FORD", model: "RANGER XLT", location: "San Pablo City", imagePath: "/images/transactions/2020 Ford Ranger XLT.webp", purchasedAt: "2026-05-24" },
   { year: "2023", brand: "FORD", model: "RANGER RAPTOR", location: "Baguio City", imagePath: "/images/transactions/2023 Ford Ranger Raptor.webp", purchasedAt: "2026-05-25" },
   { year: "2019", brand: "Mercedes-Benz", model: "C 180", location: "Quezon City", imagePath: "/images/transactions/2019 Mercedes-Benz C 180.webp", purchasedAt: "2026-06-08" },
   { year: "2017", brand: "Kia", model: "Grand Carnival", location: "Antipolo City", imagePath: "/images/transactions/2017 Kia Grand Carnival.webp", purchasedAt: "2026-06-14" },
