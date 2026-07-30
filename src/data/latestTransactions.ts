accf9e5f38d6da6b346cbc451030a08dc740d69f
red <destinydn26@gmail.com>
2026-07-30 17:20:03 +0800

    Add daily transaction images

--- src/data/latestTransactions.ts
+++ src/data/latestTransactions.ts
@@ -1,8 +1,15 @@
 export const latestTransactions = [
-  { year: "2016", brand: "MAZDA", model: "MX-5 Miata", location: "Cavite City", imagePath: "/images/transactions/2016 Mazda MX-5.webp", purchasedAt: "2026-03-15" },
-  { year: "2012", brand: "BMW", model: "520d", location: "San Pablo City", imagePath: "/images/transactions/2012 BMW 5 Series 520d.webp", purchasedAt: "2026-03-24" },
-  { year: "2023", brand: "FORD", model: "RANGER RAPTOR", location: "Baguio City", imagePath: "/images/transactions/2023 Ford Ranger Raptor.webp", purchasedAt: "2026-04-08" },
-  { year: "2019", brand: "Mercedes-Benz", model: "C 180", location: "Quezon City", imagePath: "/images/transactions/2019 Mercedes-Benz C 180.webp", purchasedAt: "2026-04-08" },
-  { year: "2017", brand: "Kia", model: "Grand Carnival", location: "Antipolo City", imagePath: "/images/transactions/2017 Kia Grand Carnival.webp", purchasedAt: "2026-04-26" },
+  { year: "2016", brand: "MAZDA", model: "MX-5 Miata", location: "Cavite City", imagePath: "/images/transactions/2016 Mazda MX-5.webp", purchasedAt: "2026-05-15" },
+  { year: "2020", brand: "FORD", model: "RANGER XLT", location: "San Pablo City", imagePath: "/images/transactions/2012 2020 Ford Ranger XLT.webp", purchasedAt: "2026-05-24" },
+  { year: "2023", brand: "FORD", model: "RANGER RAPTOR", location: "Baguio City", imagePath: "/images/transactions/2023 Ford Ranger Raptor.webp", purchasedAt: "2026-05-25" },
+  { year: "2019", brand: "Mercedes-Benz", model: "C 180", location: "Quezon City", imagePath: "/images/transactions/2019 Mercedes-Benz C 180.webp", purchasedAt: "2026-06-08" },
+  { year: "2017", brand: "Kia", model: "Grand Carnival", location: "Antipolo City", imagePath: "/images/transactions/2017 Kia Grand Carnival.webp", purchasedAt: "2026-06-14" },
+  { year: "2018", brand: "Toyota", model: "Fortuner 2.4 G", location: "Marilao City", imagePath: "/images/transactions/2018 Toyota Fortuner 2.4 G.webp", purchasedAt: "2026-06-20" },
+  { year: "2017", brand: "Hyundai", model: "Tucson 2.0 CRDi", location: "Pasig City", imagePath: "/images/transactions/2017 Hyundai Tucson 2.0 CRDi.webp", purchasedAt: "2026-06-26" },
+  { year: "2021", brand: "Suzuki", model: "S-Presso", location: "Valenzuela City", imagePath: "/images/transactions/2021 Suzuki S-Presso.webp", purchasedAt: "2026-07-15" },
+  { year: "2022", brand: "Toyota", model: "Corolla Cross", location: "Taytay City", imagePath: "/images/transactions/2022 Toyota Corolla Cross.webp", purchasedAt: "2026-07-19" },
+  { year: "2014", brand: "Ford", model: "Explorer", location: "Antipolo City", imagePath: "/images/transactions/2014 Ford Explorer.webp", purchasedAt: "2026-07-22" },
+  { year: "2019", brand: "Nissan", model: "Terra", location: "Las Piñas City", imagePath: "/images/transactions/2019 Nissan Terra.webp", purchasedAt: "2026-07-25" },
+  { year: "2016", brand: "Toyota", model: "Yaris", location: "Makati City", imagePath: "/images/transactions/2016 Toyota Yaris.webp", purchasedAt: "2026-07-28" },
 ]
 
